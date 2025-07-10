61dd1d194299d721362598d7b869ad6f4a573673
kohyeokjae <kohj0686@naver.com>
2025-07-11 03:27:04 +0900

    feat: branch khj

--- src/main/java/com/example/bookhub_back/repository/BranchRepository.java
+++ src/main/java/com/example/bookhub_back/repository/BranchRepository.java
@@ -1,16 +1,15 @@
 package com.example.bookhub_back.repository;
 
 import com.example.bookhub_back.entity.Branch;
-import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
     boolean existsByBranchName(String branchName);
 
     @Query("SELECT b FROM Branch b WHERE (:branchLocation IS NULL OR b.branchLocation LIKE CONCAT('%', :branchLocation, '%'))")
-    List<Branch> searchBranch(@Param("branchLocation") String branchLocation);
+    Page<Branch> searchBranch(@Param("branchLocation") String branchLocation, Pageable pageable);
 }
