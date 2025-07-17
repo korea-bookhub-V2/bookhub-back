28df2d88c3c28a2dd4391b2c7fe773919df04838
choiseoyoon <chsu0904@naver.com>
2025-07-17 11:13:21 +0900

    feature/stock-manager-csy

--- src/main/java/com/example/bookhub_back/service/stock/StockService.java
+++ src/main/java/com/example/bookhub_back/service/stock/StockService.java
@@ -4,11 +4,12 @@ import com.example.bookhub_back.dto.PageResponseDto;
 import com.example.bookhub_back.dto.ResponseDto;
 import com.example.bookhub_back.dto.stock.request.StockUpdateRequestDto;
 import com.example.bookhub_back.dto.stock.response.StockResponseDto;
+import com.example.bookhub_back.security.auth.EmployeePrincipal;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 
 public interface StockService {
-    ResponseDto<Void> updateStock(Long stockId, @Valid StockUpdateRequestDto dto);
+    ResponseDto<Void> updateStock(EmployeePrincipal employeePrincipal, Long stockId, @Valid StockUpdateRequestDto dto);
 
     ResponseDto<PageResponseDto<StockResponseDto>> getFilteredStocks(@Min(0) int page, @Min(1) int size, Long branchId, String bookTitle);
 
