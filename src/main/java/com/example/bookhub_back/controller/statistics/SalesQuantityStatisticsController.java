764614a3257be9c0edfa164ec8cd79838aec0968
Bottomdeal <zpalqm1357@nate.com>
2025-08-01 11:27:11 +0900

    refactor: statistics sjw

--- src/main/java/com/example/bookhub_back/controller/statistics/SalesQuantityStatisticsController.java
+++ src/main/java/com/example/bookhub_back/controller/statistics/SalesQuantityStatisticsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(ApiMappingPattern.BASIC_API)
+@RequestMapping()
 @RequiredArgsConstructor
 public class SalesQuantityStatisticsController {
     private final SalesQuantityStatisticsService salesQuantityStatisticsService;
